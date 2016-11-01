c8ebb22cd3f4126ca44ac201cd752fb8fa96c12a
Kyle Minshall <kyleminshall@gmail.com>
2016-11-01 03:15:59 -0700

    Converting to crawler class and updating commandline arguments

--- crawler.py
+++ crawler.py
@@ -1,179 +1,200 @@
 import urllib3
 from bs4 import BeautifulSoup
 from queue import Queue
+import argparse
 import threading
 import sys
 import os
 
 
 # Disable warnings for https
-#urllib3.disable_warnings()
-
-# Hashset for visited links
-hashSet = set()
-# Queue to handle links
-links = Queue(5000)
-# Number of links visited
-visited = 0
-# Number of hops
-hops = 0
-# directory
-directory = ""
-keep_going = True
+urllib3.disable_warnings()
 
 #link class has url and depth
 class Link ():
+    ''' Plain class to store links including the url and their depth
+
+        depth is how far from the root the link was discovered
+    '''
     def __init__(self,url,depth):
         #initialize
         self.url = url
         self.depth = depth
 
-def crawl(link):
-    ''' When given a link and a hop level, will parse the html for the given
-        link, grab all of the hrefs, and throw them in a queue to be parsed
-        later.
+class Crawler():
 
-        Hop level is how deep the crawler will go.
-        e.g. hop level of 1 means it will only parse the links found on the
-        given page. hop 2 means all the links of all sub pages of the initial
-        link, etc.
-    '''
-    global hashSet
+    def __init__(self, hops, directory, num_pages):
+        ''' Initialize the crawler for crawling the web
+        '''
+        # Tells the crawler when to stop
+        self.keep_going = True
+        # Hashset for visited links
+        self.visited_links = set()
+        # Queue to handle links
+        self.links_queue = Queue(5000)
+        # Number of links visited
+        self.visited = 0
+        # Number of hops
+        self.hops = hops
+        # directory
+        self.directory = directory
+        # max number of pages to crawl
+        self.num_pages = num_pages
+        # Setup urllib 3
+        self.http = urllib3.PoolManager(timeout=2.0)
 
-    # If in hashset, return
-    if (link.url in hashSet):
-        return
+    def has_visited(self, url):
+        ''' Check if a link has been visited before
+        '''
+        return url in self.visited_links
 
-    # Setup urllib3
-    http = urllib3.PoolManager(timeout=2.0)
-    r = http.request('GET', link.url)
-    # Decode everything with utf8 support
-    html = r.data
-    # Parse the document so it can be processed
-    soup = BeautifulSoup(html, 'html.parser')
+    def enqueue_link(self, link):
+        ''' Add a link to the queue externally
+        '''
+        self.links_queue.put(link)
 
-    
-     
-    # Parse and store page    
-    scheme = ""
-    domain = ""
-    endIndex = link.url.index("/",8)
-    if (link.url[:7] == "http://"):
-        scheme = "http://"
-        domain = link.url[7:endIndex]
-    else:
-        scheme = "https://"
-        domain = link.url[8:endIndex] 
+    def start_crawling(self):
+        ''' Starts crawling with specified number of threads.
 
-    domainDir = "{}/{}".format(directory,domain)
-    # If domain doesn't have a dir in output_dir, make one 
-    if ( not os.path.exists(domainDir)):
-        os.makedirs(domainDir)
-    
-    i = 0
-    while os.path.exists("{}/{}_%s.html".format(domainDir,domain) % i):
-        i+=1
+            Once queue is empty, stops crawling.
+        '''
+        for i in range(10):
+            t = threading.Thread(target=self.crawler_daemon)
+            t.daemon = True
+            t.start()
+        self.links_queue.join()
 
-    # Download to dir  
-    with open("{}/{}_%s.html".format(domainDir,domain) % i,'w') as fid:
-        fid.write(str(html))
+    def crawl(self, link):
+        ''' When given a link and a hop level, will parse the html for the given
+            link, grab all of the hrefs, and throw them in a queue to be parsed
+            later.
 
-    hashSet.add(link.url)
-    
-    if (hops - link.depth <= 0):
-        return
-        
-    # Adds link to the queue
-    for pageLink in soup.find_all('a'):
-        global keep_going
-        if (links.full()):
-            keep_going = False
-        if (not keep_going):
-            break  
-        url = pageLink.get('href')
+            Hop level is how deep the crawler will go.
+            e.g. hop level of 1 means it will only parse the links found on the
+            given page. hop 2 means all the links of all sub pages of the initial
+            link, etc.
+        '''
+        # If in visited_links, return
+        if self.has_visited(link.url):
+            return
 
-        if ( url is None):
-            continue
+        # Setup urllib3
+        r = self.http.request('GET', link.url)
+        # Decode everything with utf8 support
+        html = r.data
+        # Parse the document so it can be processed
+        soup = BeautifulSoup(html, 'html.parser')
 
-        # Filter out or fix malformed/incomplete links
-        if (url.startswith('/')):
-            url = scheme + domain + url
+        # Parse and store page
+        scheme = ""
+        domain = ""
+        endIndex = link.url.index("/",8)
+        if (link.url[:7] == "http://"):
+            scheme = "http://"
+            domain = link.url[7:endIndex]
+        else:
+            scheme = "https://"
+            domain = link.url[8:endIndex]
 
-        if (not url.startswith('http://')):
-            continue
-  
-        #check if url is in hashset
-        if (url not in hashSet):
-            links.put( Link(url, link.depth +1))
+        domainDir = "{}/{}".format(self.directory, domain)
+        # If domain doesn't have a dir in output_dir, make one
+        if not os.path.exists(domainDir):
+            os.makedirs(domainDir)
 
-def crawler():
-  while True:  
-        global visited
-        print("Crawling and parsing #" + str(visited)) 
-        visited += 1
-        link = links.get()
-        try:
-            crawl(link)
-        except Exception as e:
-            print("Error: {}".format(e))
-            pass
-        finally:
-            links.task_done()
-            
+        i = 0
+        while os.path.exists("{}/{}_{}.html".format(domainDir, domain, i)):
+            i += 1
+
+        # Download to dir
+        with open("{}/{}_{}.html".format(domainDir, domain, i),'w') as fid:
+            fid.write(str(html))
+
+        # Mark the page as visited
+        self.visited_links.add(link.url)
+
+        if self.hops - link.depth <= 0:
+            return
+
+        # Adds link to the queue
+        for pageLink in soup.find_all('a'):
+            if len(self.visited_links) >= self.num_pages:
+                self.keep_going = False
+            if not self.keep_going:
+                break
+
+            url = pageLink.get('href')
+            if url is None:
+                continue
+
+            # Filter out or fix malformed/incomplete links
+            if url.startswith('/'):
+                url = scheme + domain + url
+
+            if not url.startswith('http://'):
+                continue
+
+            #check if url is in hashset
+            if url not in self.visited_links:
+                self.links_queue.put(Link(url, link.depth+1))
+
+    def crawler_daemon(self):
+        ''' Takes a link off the parse queue, crawls and saves the page,
+            and then marks the link as completed.
+        '''
+        while True:
+            link = self.links_queue.get()
+            try:
+                self.crawl(link)
+            except Exception as e:
+                print('Error: {}'.format(e))
+                pass
+            finally:
+                self.links_queue.task_done()
+
 
 def main():
     ''' Sets up the initial queue with the seeds provided and crawls the links
         until all of the levels are exhausted
     '''
-    global hops
-    global directory
-    # Make sure they provide a seeds file
-    if (len(sys.argv) <= 1):
-        print("{}\n\n{}".format(
-            'Usage:',
-            'python3 crawler.py <seed-File:seed.txt> <num-pages:1000> <hops-away: 6> <output-dir>',
-        ))
-        sys.exit(1)
-    
-    # Seeds file is the first argument (not the python file name)
-    links_list = sys.argv[1]
-    num_pages = int(sys.argv[2])
-    hops = int(sys.argv[3])   
-    output_dir = sys.argv[4] 
-    directory = output_dir
+    # Set up arguments parser for variables we need
+    parser = argparse.ArgumentParser(prog='CS172 Web Crawler',
+                                     description='Web Crawler')
+    parser.add_argument('-s', '--seed-file', dest='seed_file', type=str,
+                        required=True, nargs='?',
+                        help='List of files to start the crawler on')
+    parser.add_argument('-n', '--num-pages', dest='num_pages', type=int,
+                        default=1000, required=True, nargs='?',
+                        help='Total number of pages to crawl before stopping')
+    parser.add_argument('-p', '--hops', dest='hops', type=int,
+                        default=0, required=True, nargs='?',
+                        help='How deep the crawler will go down a link path')
+    parser.add_argument('-o', '--output-directory', dest='output_dir', type=str,
+                        default='crawler_pages', required=True, nargs='?',
+                        help=('The directory where you want the text of the'
+                              'pages to be saved to')
+                        )
+    args = parser.parse_args()
+
+    # Create the crawler to do the crawling
+    crawler = Crawler(hops=args.hops,
+                      directory=args.output_dir,
+                      num_pages=args.num_pages)
 
     # Create output_dir if it doesn't exist
-    if ( not os.path.exists(output_dir)):
-        os.makedirs(output_dir)
-
-    # If hops equals 0, return
-    if ( hops == 0):
-        return
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
 
     # Open the file and put num_pages of lines in the queue
-    with open(links_list, 'r') as f:
-        pages = 0
+    with open(args.seed_file, 'r') as f:
         for line in f:
-            if (pages < int(num_pages)):
-                links.put( Link(line.rstrip(),1))
-                pages += 1
-            else:
-                break
-    
+            crawler.enqueue_link(Link(line.rstrip(), 1))
 
-    for i in range(100):
-        t = threading.Thread(target=crawler)
-        t.daemon = True
-        t.start()
+    # We're all set. Start crawling
+    crawler.start_crawling()
 
-    links.join()
-    
-    
-    
-    print (len(hashSet))
     # Printing out all of the links
-    print("\n".join(str(e) for e in hashSet))
+    print("\n".join(str(e) for e in crawler.visited_links))
 
-if (__name__ == '__main__'):
+if __name__ == '__main__':
     main()
 
