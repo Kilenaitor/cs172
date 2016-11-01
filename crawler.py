f1e352d7b74ed996e616412a81a5dd9aa14048cb
Kyle Minshall <kyleminshall@gmail.com>
2016-11-01 04:20:41 -0700

    Fixing style, adjusting num_pages val, adding parser

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 import urllib3
 from bs4 import BeautifulSoup
 from queue import Queue
+import argparse
 import threading
 import sys
 import os
@@ -15,6 +16,8 @@ hashSet = set()
 links = Queue(5000)
 # Number of links visited
 visited = 0
+# Max number of pages to crawl
+num_pages = 0
 # Number of hops
 hops = 0
 # directory
@@ -52,9 +55,7 @@ def crawl(link):
     # Parse the document so it can be processed
     soup = BeautifulSoup(html, 'html.parser')
 
-    
-     
-    # Parse and store page    
+    # Parse and store page
     scheme = ""
     domain = ""
     endIndex = link.url.index("/",8)
@@ -63,53 +64,56 @@ def crawl(link):
         domain = link.url[7:endIndex]
     else:
         scheme = "https://"
-        domain = link.url[8:endIndex] 
+        domain = link.url[8:endIndex]
 
     domainDir = "{}/{}".format(directory,domain)
-    # If domain doesn't have a dir in output_dir, make one 
-    if ( not os.path.exists(domainDir)):
+    # If domain doesn't have a dir in output_dir, make one
+    if not os.path.exists(domainDir):
         os.makedirs(domainDir)
-    
-    i = 0
-    while os.path.exists("{}/{}_%s.html".format(domainDir,domain) % i):
-        i+=1
 
-    # Download to dir  
-    with open("{}/{}_%s.html".format(domainDir,domain) % i,'w') as fid:
+    i = 0
+    while os.path.exists("{}/{}_{}.html".format(domainDir,domain, i)):
+        i += 1
+
+    # Download to dir
+    with open("{}/{}_{}.html".format(domainDir,domain, i), 'w') as fid:
         fid.write(str(html))
 
     hashSet.add(link.url)
-    
-    if (hops - link.depth <= 0):
+
+    if hops - link.depth <= 0:
         return
-        
+
     # Adds link to the queue
     for pageLink in soup.find_all('a'):
         global keep_going
-        if (links.full()):
+        global num_pages
+        if len(hashSet) >= num_pages:
+            print("-------------------------------- DONE -------------------------------")
             keep_going = False
-        if (not keep_going):
-            break  
+        if not keep_going:
+            break
         url = pageLink.get('href')
 
-        if ( url is None):
+        if url is None:
             continue
 
         # Filter out or fix malformed/incomplete links
-        if (url.startswith('/')):
+        if url.startswith('/'):
             url = scheme + domain + url
 
-        if (not url.startswith('http://')):
+        if not url.startswith('http://'):
             continue
-  
-        #check if url is in hashset
-        if (url not in hashSet):
-            links.put( Link(url, link.depth +1))
+
+        # Check if url is in hashset
+        if url not in hashSet:
+            links.put(Link(url, link.depth +1), timeout=2.0)
+
 
 def crawler():
-  while True:  
+    while True:
         global visited
-        print("Crawling and parsing #" + str(visited)) 
+        print("Crawling and parsing #" + str(visited))
         visited += 1
         link = links.get()
         try:
@@ -119,7 +123,7 @@ def crawler():
             pass
         finally:
             links.task_done()
-            
+
 
 def main():
     ''' Sets up the initial queue with the seeds provided and crawls the links
@@ -127,39 +131,36 @@ def main():
     '''
     global hops
     global directory
-    # Make sure they provide a seeds file
-    if (len(sys.argv) <= 1):
-        print("{}\n\n{}".format(
-            'Usage:',
-            'python3 crawler.py <seed-File:seed.txt> <num-pages:1000> <hops-away: 6> <output-dir>',
-        ))
-        sys.exit(1)
-    
-    # Seeds file is the first argument (not the python file name)
-    links_list = sys.argv[1]
-    num_pages = int(sys.argv[2])
-    hops = int(sys.argv[3])   
-    output_dir = sys.argv[4] 
-    directory = output_dir
+    global num_pages
+    parser = argparse.ArgumentParser(prog='CS172 Web Crawler',
+                                     description='Web Crawler')
+    parser.add_argument('-s', '--seed-file', dest='seed_file', type=str,
+                        required=True, nargs='?',
+                        help='List of files to start the crawler on')
+    parser.add_argument('-n', '--num-pages', dest='num_pages', type=int,
+                        default=1000, required=True, nargs='?',
+                        help='Total number of pages to crawl before stopping')
+    parser.add_argument('-p', '--hops', dest='hops', type=int,
+                        default=0, required=True, nargs='?',
+                        help='How deep the crawler will go down a link path')
+    parser.add_argument('-o', '--output-directory', dest='output_dir', type=str,
+                        default='crawler_pages', required=True, nargs='?',
+                        help=('The directory where you want the text of the'
+                              'pages to be saved to')
+                        )
+    args = parser.parse_args()
+    directory = args.output_dir
+    hops = args.hops
+    num_pages = args.num_pages
 
     # Create output_dir if it doesn't exist
-    if ( not os.path.exists(output_dir)):
-        os.makedirs(output_dir)
-
-    # If hops equals 0, return
-    if ( hops == 0):
-        return
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
 
     # Open the file and put num_pages of lines in the queue
-    with open(links_list, 'r') as f:
-        pages = 0
+    with open(args.seed_file, 'r') as f:
         for line in f:
-            if (pages < int(num_pages)):
-                links.put( Link(line.rstrip(),1))
-                pages += 1
-            else:
-                break
-    
+            links.put(Link(line.rstrip(),1), timeout=2.0)
 
     for i in range(100):
         t = threading.Thread(target=crawler)
@@ -167,13 +168,10 @@ def main():
         t.start()
 
     links.join()
-    
-    
-    
-    print (len(hashSet))
+
     # Printing out all of the links
     print("\n".join(str(e) for e in hashSet))
 
-if (__name__ == '__main__'):
+if __name__ == '__main__':
     main()
 
